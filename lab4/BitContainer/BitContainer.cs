1d6012d5e9d70af419443ef1e1f4637c86b02ae9
Nazeebo <romanov0902@gmail.com>
2019-05-29 13:36:27 +0200

    lab4 final

--- lab4/BitContainer/BitContainer.cs
+++ lab4/BitContainer/BitContainer.cs
@@ -31,15 +31,16 @@ namespace BitContainer {
         }
 
         private void setBit(int position, int bit) {
-            if (position >= Lenght &&  position < 0)
-                throw new Exception("Out of range");
-            else {
+            if (position >= Lenght ||  position < 0)
+                throw new IndexOutOfRangeException("Out of range");
+            if (bit != 0 && bit != 1) 
+                throw new ArgumentOutOfRangeException("Incorrect value");
+
                 int place = position / 8;
                 int offset = position % 8;
                 int currByte = list[place];
                 currByte &= ~(1 << offset); //cбрасываем бит на offset 
                 list[place] = Convert.ToByte((currByte | (bit << offset)) & 0xff); //записываем новое значение
-            }
         }
 
         private void setBit(int position, bool bit) {
@@ -48,7 +49,7 @@ namespace BitContainer {
         }
 
         private int getBit(int position) {
-            if (position >= Lenght && position < 0)
+            if (position >= Lenght || position < 0)
                 throw new IndexOutOfRangeException("Out of range");
             else {
                 int place = position / 8;
@@ -66,7 +67,11 @@ namespace BitContainer {
         }
 
         public void Insert(int place, int bit) {
-            ++lenght;
+            if (place >= Lenght ||  place < 0)
+                throw new IndexOutOfRangeException("Out of range");
+            if (bit != 0 && bit != 1) 
+                throw new ArgumentOutOfRangeException("Incorrect value");
+            pushBit(this[Lenght-1]);
             for (int i = place; i < Lenght - 1; ++i)
                 this[i + 1] = this[i];
             this[place] = bit;
@@ -85,19 +90,19 @@ namespace BitContainer {
         }
 
         public override string ToString() {
-            string str = "";
+            StringBuilder str = new StringBuilder();
             int bytes = (Lenght - 1) / 8;
             int restInLast = (Lenght - 1) % 8;
             for (int i = 0; i <= bytes - 1; ++i) {
                 for (int j = 7; j >= 0; --j)
-                    str += this[i*8 + j];
-                str += " ";
+                    str.Append(this[i*8 + j]);
+                str.Append(" ");
             }
 
             for (int i = restInLast; i >= 0; --i)
-                str += this[bytes * 8 + i];
+                str.Append(this[bytes * 8 + i]); 
                 
-            return str;
+            return str.ToString();
         }
 
         public IEnumerator<bool> GetEnumerator() {
@@ -115,7 +120,6 @@ namespace BitContainer {
             set{
                 if (value == 1 || value == 0)
                     setBit(position, value);
-                else throw new ArgumentOutOfRangeException("Incorrect to write wrong value");
             }
         }
 
